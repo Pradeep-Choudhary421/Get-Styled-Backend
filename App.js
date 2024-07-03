68bcad24f38954d6c0c88a0ffab245526b0bc388
103326421 <pradeep9783552320@gmail.com>
2024-07-03 12:30:16 +0530

    git commited origin

--- App.js
+++ App.js
@@ -11,7 +11,7 @@ const cookieParser = require("cookie-parser");
 require("dotenv").config();
 
 const corsOptions = {
-  origin: "*",
+  origin: ["https://get-styled.vercel.app/","http://localhost:5173/"],
   methods: ["GET", "POST"],
   credentials: true,
   optionsSuccessStatus: 204,
