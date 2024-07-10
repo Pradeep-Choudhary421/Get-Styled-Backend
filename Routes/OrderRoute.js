652d7834b74b6e2d1965ca0ffb543943f739dacf
103326421 <pradeep9783552320@gmail.com>
2024-07-10 17:34:51 +0530

    deleteCart

--- Routes/OrderRoute.js
+++ Routes/OrderRoute.js
@@ -1,7 +1,8 @@
 const express = require("express")
-const { addToCart, getcarItems} = require('../Controllers/OrderContorller');
+const { addToCart, getcarItems, deleteCartItem} = require('../Controllers/OrderContorller');
 const {isAuthenticated} = require("../MiddleWare/Auth")
 const router = express.Router();
 router.post("/addToCart",isAuthenticated, addToCart)
 router.get("/getcartItems",isAuthenticated, getcarItems)
+router.delete("/deletecartItems",isAuthenticated, deleteCartItem)
 module.exports = router;
