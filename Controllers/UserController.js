e42c7598adbd6dad120ff40cc6a06e51b1254d8e
103326421 <pradeep9783552320@gmail.com>
2024-07-11 18:17:35 +0530

    nodemailer

--- Controllers/UserController.js
+++ Controllers/UserController.js
@@ -1,6 +1,7 @@
 const UserSch = require("../Models/UserSchema");
 const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
+const nodemailer = require("nodemailer");
 require("dotenv").config();
 
 const createUser = async (req, res) => {
@@ -55,25 +56,30 @@ const getallusers = async (req, res) => {
   }
 };
 const updateUser = async (req, res) => {
-  const id = req.params.id;
-  const updatedUser = req.body;
+  const { email, newPassword } = req.body;
+
+  if (!newPassword) {
+    return res.status(400).json({
+      message: "New password is required",
+    });
+  }
 
   try {
-    const emailExists = await UserSch.findOne({
-      email: req.body.email,
-    });
-    const user = await UserSch.findOne({
-      _id: id,
-    });
+    const user = await UserSch.findOne({ email });
+
     if (!user) {
       return res.status(404).json({
         message: "User not found",
       });
     }
-    await UserSch.findByIdAndUpdate(id, updatedUser);
+
+    const hashedpassword = await bcrypt.hash(newPassword, 10);
+    user.password = hashedpassword;
+
+    await user.save();
     return res.status(200).json({
-      message: "User Updated Successfully",
-      result: updatedUser,
+      message: "Password Updated Successfully",
+      result: user,
     });
   } catch (err) {
     return res.status(500).json({
@@ -139,4 +145,46 @@ const login = async (req, res) => {
     });
   }
 };
-module.exports = { createUser, getallusers, updateUser, deleteUser, login };
+
+
+const sendOtp = async (req, res) => {
+  const { email } = req.body;
+  const user = await UserSch.findOne({ email });
+  if (!user) {
+    return res.status(400).json({
+      success: false,
+      message: "User not found",
+    });
+  }
+  let otp = Math.floor(1000 + Math.random() * 900000);
+  let mailTransporter = nodemailer.createTransport({
+    service: "gmail",
+    auth: {
+      user: "pradeep9783552320@gmail.com",
+      pass: "amtb ohys vkcl tgya",
+    },
+  });
+  let mailingDeatils = {
+    from: "pradeep9783552320@gmail.com",
+    to: email,
+    subject: "OTP to reset your password",
+    text: `Your OTP is ${otp}`,
+  };
+
+  mailTransporter.sendMail(mailingDeatils, function (err, data) {
+    if (err) {
+      res.stauts(500).json({
+        success: false,
+        message: err.message,
+      });
+    } else {
+      res.status(200).json({
+        success: true,
+        message: "OTP sent to your email",
+        otp,
+        user,
+      });
+    }
+  });
+};
+module.exports = { createUser, getallusers, updateUser, deleteUser, login, sendOtp };
--- Routes/UserRoute.js
+++ Routes/UserRoute.js
@@ -4,7 +4,8 @@ const {isAuthenticated} = require("../MiddleWare/Auth")
 const router = express.Router();
 router.post("/createUser", createUser)
 router.post("/login", login);   
-router.get("/getAllUser", isAuthenticated , getallusers)
-router.put("/updateUser/:id", updateUser)
+router.post("/sendOtp", sendOtp );   
+router.get("/getAllUser" , getallusers)
+router.post("/updateUser", updateUser)
 router.delete("/deleteUser/:id", deleteUser)
 module.exports = router;
